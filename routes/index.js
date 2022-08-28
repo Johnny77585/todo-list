311b1d86ddc2b47c4060c994472edc0b122edf5c
Johnny77585 <jkuo2188@gmail.com>
2022-08-28 09:54:15 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
+
+// 準備引入路由模組
 const router = express.Router()
 const todos = require('./modules/todos')
-// 準備引入路由模組
-// 匯出路由器
+const users = require('./modules/user')
 const home = require('./modules/home')
+
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
+
+// 匯出路由器
 module.exports = router
