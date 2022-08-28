b5cad25db30febb01e5924486f2e96b5e966c78a
Johnny77585 <jkuo2188@gmail.com>
2022-08-28 11:48:59 +0800

    feat: add user registration feature

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const todos = require('./modules/todos')
 const users = require('./modules/user')
 const home = require('./modules/home')
 
-// 將網址結構符合 / 字串的 request 導向 home 模組 
+// 將網址結構符合 / 字串的 request 導向模組 
 router.use('/', home)
 router.use('/todos', todos)
 router.use('/users', users)
