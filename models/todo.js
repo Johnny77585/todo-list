f6a7dcd09819a9786edd5fa5953b2b72b615bf6e
Johnny77585 <jkuo2188@gmail.com>
2022-08-29 15:19:45 +0800

    feat: add relations between User and Todo models

--- models/todo.js
+++ models/todo.js
@@ -5,9 +5,15 @@ const todoSchema = new Schema({
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   },
-   isDone: {
+  isDone: {
     type: Boolean,
     default: false//預設完成狀態為false
+  },
+  userId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,//定義 userId 這個項目是一個 ObjectId，也就是它會連向另一個資料物件
+    ref: 'User',//定義參考對象是 User model
+    index: true,
+    required: true
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
