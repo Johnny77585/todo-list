4ca1ef27668c2a1e5e225cfc5e7b6233b9ea4ddd
Johnny77585 <jkuo2188@gmail.com>
2022-09-01 13:10:39 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/user')
+const auth = require('./modules/auth')
+
 const { authenticator } = require('../middleware/auth')  // 掛載 middleware
+
 router.use('/todos', authenticator, todos) // 加入驗證程序
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home) // 加入驗證程序
 
 // 匯出路由器
